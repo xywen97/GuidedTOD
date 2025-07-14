98afa8794f858219ec1561a71cedaf7d2d2c951f
xywen97 <xywen22@cse.cuhk.edu.hk>
2025-07-14 10:32:23 +0800

    update files

--- train_abcd_ast_woaction_flow.sh
+++ train_abcd_ast_woaction_flow.sh
@@ -1,36 +1,36 @@
 # set cuda device
 
 # 10p
-# echo "abcdASTWOActionFlow10P"
-# export CUDA_VISIBLE_DEVICES=0,1
-# python train.py --experiment_name abcdASTWOActionFlow10P \
-#  --model_name_or_path t5-base \
-#   --do_train \
-#   --do_eval \
-#   --do_predict \
-#   --num_train_epochs 100 \
-#   --train_file ./data/processed/train_AST_abcd_woaction_flow_10p.json \
-#   --validation_file ./data/processed/dev_AST_abcd_woaction_flow_10p.json \
-#   --test_file ./data/processed/test_AST_abcd_woaction_flow_10p.json \
-#   --text_column input \
-#   --summary_column target \
-#   --per_device_train_batch_size 16 \
-#   --per_device_eval_batch_size 16 \
-#   --predict_with_generate \
-#   --output_dir ./results/ \
-#   --save_strategy epoch \
-#   --source_prefix "Predict AST: " \
-#   --max_source_length 1024 \
-#   --max_target_length 256 \
-#   --val_max_target_length 256 \
-#   --learning_rate 5e-5 \
-#   --warmup_steps 500 \
-#   --use_ast_metrics \
-#   --use_fast_tokenizer False \
-#   --num_beams 4
+echo "abcdASTWOActionFlow10P"
+export CUDA_VISIBLE_DEVICES=0,1
+python train.py --experiment_name abcdASTWOActionFlow10P \
+ --model_name_or_path t5-base \
+  --do_train \
+  --do_eval \
+  --do_predict \
+  --num_train_epochs 100 \
+  --train_file ./data/processed/train_AST_abcd_woaction_flow_10p.json \
+  --validation_file ./data/processed/dev_AST_abcd_woaction_flow_10p.json \
+  --test_file ./data/processed/test_AST_abcd_woaction_flow_10p.json \
+  --text_column input \
+  --summary_column target \
+  --per_device_train_batch_size 16 \
+  --per_device_eval_batch_size 16 \
+  --predict_with_generate \
+  --output_dir ./results/ \
+  --save_strategy epoch \
+  --source_prefix "Predict AST: " \
+  --max_source_length 1024 \
+  --max_target_length 256 \
+  --val_max_target_length 256 \
+  --learning_rate 5e-5 \
+  --warmup_steps 500 \
+  --use_ast_metrics \
+  --use_fast_tokenizer False \
+  --num_beams 4
 
-# # delete the saved model
-# rm -rf results/abcdASTWOActionFlow10P_input_target_t5-base/checkpoint-*
+# delete the saved model
+rm -rf results/abcdASTWOActionFlow10P_input_target_t5-base/checkpoint-*
 
 
 # export CUDA_VISIBLE_DEVICES=0,1
@@ -96,36 +96,36 @@
 # rm -rf results/abcdASTWOActionFlowHalf_input_target_t5-base/checkpoint-*
 
 # all
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-echo "abcdASTWOActionFlowAll"
-python train.py --experiment_name abcdASTWOActionFlowAll \
- --model_name_or_path t5-base \
-  --do_train \
-  --do_eval \
-  --do_predict \
-  --num_train_epochs 100 \
-  --train_file ./data/processed/train_AST_abcd_woaction_flow_all.json \
-  --validation_file ./data/processed/dev_AST_abcd_woaction_flow_all.json \
-  --test_file ./data/processed/test_AST_abcd_woaction_flow_all.json \
-  --text_column input \
-  --summary_column target \
-  --per_device_train_batch_size 32 \
-  --per_device_eval_batch_size 32 \
-  --predict_with_generate \
-  --output_dir ./results/ \
-  --save_strategy epoch \
-  --source_prefix "Predict AST: " \
-  --max_source_length 1024 \
-  --max_target_length 256 \
-  --val_max_target_length 256 \
-  --learning_rate 5e-5 \
-  --warmup_steps 500 \
-  --use_ast_metrics \
-  --use_fast_tokenizer False \
-  --num_beams 4
+# export CUDA_VISIBLE_DEVICES=0,1
+# echo "abcdASTWOActionFlowAll"
+# python train.py --experiment_name abcdASTWOActionFlowAll \
+#  --model_name_or_path t5-base \
+#   --do_train \
+#   --do_eval \
+#   --do_predict \
+#   --num_train_epochs 100 \
+#   --train_file ./data/processed/train_AST_abcd_woaction_flow_all.json \
+#   --validation_file ./data/processed/dev_AST_abcd_woaction_flow_all.json \
+#   --test_file ./data/processed/test_AST_abcd_woaction_flow_all.json \
+#   --text_column input \
+#   --summary_column target \
+#   --per_device_train_batch_size 32 \
+#   --per_device_eval_batch_size 32 \
+#   --predict_with_generate \
+#   --output_dir ./results/ \
+#   --save_strategy epoch \
+#   --source_prefix "Predict AST: " \
+#   --max_source_length 1024 \
+#   --max_target_length 256 \
+#   --val_max_target_length 256 \
+#   --learning_rate 5e-5 \
+#   --warmup_steps 500 \
+#   --use_ast_metrics \
+#   --use_fast_tokenizer False \
+#   --num_beams 4
 
-# delete the saved model
-rm -rf results/abcdASTWOActionFlowAll_input_target_t5-base/checkpoint-*
+# # delete the saved model
+# rm -rf results/abcdASTWOActionFlowAll_input_target_t5-base/checkpoint-*
 
 
 # 10p
